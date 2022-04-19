5d951e748ddd7ce754b5b1f4489dcaa38b50030a
Filip Rybczyński <filipryba@gmail.com>
2022-04-20 00:44:13 +0200

    Fetching ten cities

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
-      'X-RapidAPI-Key': '1be0fcd283msh40b6164f5cd1179p152c73jsn2e00608d4e8e'
-    }
+class App extends React.Component {
+  state = {
+    cities: [],
   };
 
-  let cities;
+  options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+      "X-RapidAPI-Key": "1be0fcd283msh40b6164f5cd1179p152c73jsn2e00608d4e8e",
+    },
+  };
 
-  const letsFetch = () => {
-
-    fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions?minPopulation=100000', options)
-      .then(response => response.json())
-      .then(response => {
-        cities = response.data;
-        console.log(response)
+  letsFetch = () => {
+    fetch(
+      "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions?minPopulation=100000&limit=10",
+      this.options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          cities: response.data,
+        });
+        console.log(response);
       })
-      .catch(err => console.error(err));
-  }
-  
+      .catch((err) => console.error(err));
+  };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={letsFetch}>Let's fetch!</button>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.letsFetch}>Let's fetch!</button>
+          <ul>
+            {this.state.cities.map((city) => (
+              <li key={city.name}>
+                {city.name}, {city.country} has {city.population} inhabitants.
+              </li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
