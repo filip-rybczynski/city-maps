9823b104f661ac4aed2b61781b7a8705ae82e94f
Filip Rybczyński <filipryba@gmail.com>
2022-04-19 23:52:02 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+      'X-RapidAPI-Key': '1be0fcd283msh40b6164f5cd1179p152c73jsn2e00608d4e8e'
+    }
+  };
+
+  let cities;
+
+  const letsFetch = () => {
+
+    fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions?minPopulation=100000', options)
+      .then(response => response.json())
+      .then(response => {
+        cities = response.data;
+        console.log(response)
+      })
+      .catch(err => console.error(err));
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={letsFetch}>Let's fetch!</button>
       </header>
     </div>
   );
