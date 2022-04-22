3e11eb1b613490d4d244c924854cb6141192a705
Filip Rybczyński <filipryba@gmail.com>
2022-04-23 01:02:43 +0200

    Added controlled input and search results based on input

--- src/App.js
+++ src/App.js
@@ -1,62 +1,85 @@
 import "./App.css";
 import React from "react";
+import axios from "axios";
+import CitySelection from "./components/CitySelection/CitySelection";
 
 class App extends React.Component {
   state = {
     cities: [],
     callsRemaining: 0,
+    searchText: "",
   };
 
-  options = {
+  fetchOptions = {
     method: "GET",
+    url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
+    params: {
+      minPopulation: "100000",
+      namePrefix: this.state.searchText,
+    },
     headers: {
       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
       "X-RapidAPI-Key": "1be0fcd283msh40b6164f5cd1179p152c73jsn2e00608d4e8e",
     },
   };
 
-  letsFetch = () => {
+  handleCitySearch = (e) => {
+    const searchText = e.target.value;
+
+    this.setState({
+      searchText,
+    });
+  };
+
+  getCities = (e) => {
+    e.preventDefault();
+    
     fetch(
-      "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions?minPopulation=100000&limit=10",
-      this.options
+      `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=10000&limit=10&namePrefix=${this.state.searchText}`,
+      this.fetchOptions
     )
       .then((response) => {
-        const callsRemaining = response.headers.get('x-ratelimit-requests-remaining');
-
+        const callsRemaining = response.headers.get(
+          "x-ratelimit-requests-remaining"
+        );
         this.setState({
           callsRemaining,
         });
-
-        return response.json()})
+        return response.json();
+      })
       .then((response) => {
+        const cities = response.data;
         this.setState({
-          cities: response.data,
+          cities,
         });
-        console.dir(response);
-
       })
       .catch((err) => console.error(err));
   };
 
   render() {
-    const {cities, callsRemaining} = this.state;
+    const { cities, callsRemaining, searchText } = this.state;
 
     return (
       <div className="App">
         <section className="App-header">
-          <button onClick={this.letsFetch}>Let's fetch!</button>
+          <CitySelection
+            searchedName={searchText}
+            handleCitySearch={this.handleCitySearch}
+            getCities={this.getCities}
+          ></CitySelection>
           <ul>
-            {cities.map((city) => (
-              <li key={city.name}>
+            {cities
+            .map((city) => (
+              <li key={city.id}>
                 {city.name}, {city.country} has {city.population} inhabitants.
               </li>
             ))}
           </ul>
-        <p>
-          {callsRemaining
-            ? `You have ${callsRemaining} API calls left`
-          : null}
-        </p>
+          <p>
+            {callsRemaining
+              ? `You have ${callsRemaining} API calls left`
+              : null}
+          </p>
         </section>
       </div>
     );
---
+++ src/components/CitySelection/CitySelection.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function CitySelection(props) {
+  return (
+    <>
+      <form>
+        <label htmlFor="city-name-search">Search city</label>
+        <input
+          list="city-names"
+          id="city-name-search"
+          name="city-name-search"
+          value={props.searchedName}
+          onChange={props.handleCitySearch}
+        />
+        <datalist id="city-name">
+          <option value="Chocolate" />
+          <option value="Coconut" />
+          <option value="Mint" />
+          <option value="Strawberry" />
+          <option value="Vanilla" />
+        </datalist>
+        <button onClick={props.getCities}>Let's fetch!</button>
+      </form>
+    </>
+  );
+}
+
+export default CitySelection;
