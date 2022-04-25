f12c155a7583acc20b2631d194c9254d6d8f2e99
Filip Rybczyński <filipryba@gmail.com>
2022-04-26 00:03:59 +0200

    Automatic and debounced data fetching when filling in search field

--- src/App.js
+++ src/App.js
@@ -12,16 +12,13 @@ class App extends React.Component {
 
   fetchOptions = {
     method: "GET",
-    url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
-    params: {
-      minPopulation: "100000",
-      namePrefix: this.state.searchText,
-    },
     headers: {
       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
       "X-RapidAPI-Key": "1be0fcd283msh40b6164f5cd1179p152c73jsn2e00608d4e8e",
     },
   };
+
+  fetchTimer;
 
   handleCitySearch = (e) => {
     const searchText = e.target.value;
@@ -29,11 +26,14 @@ class App extends React.Component {
     this.setState({
       searchText,
     });
+
+    clearTimeout(this.fetchTimer);
+    this.fetchTimer = setTimeout(() => {this.getCities(e)}, 1000);
   };
 
   getCities = (e) => {
     e.preventDefault();
-    
+
     fetch(
       `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=10000&limit=10&namePrefix=${this.state.searchText}`,
       this.fetchOptions
@@ -69,11 +69,15 @@ class App extends React.Component {
           ></CitySelection>
           <ul>
             {cities
-            .map((city) => (
-              <li key={city.id}>
-                {city.name}, {city.country} has {city.population} inhabitants.
-              </li>
-            ))}
+              .filter((city) => {
+
+                return city.name.startsWith(searchText);
+              })
+              .map((city) => (
+                <li key={city.id}>
+                  {city.name}, {city.country} has {city.population} inhabitants.
+                </li>
+              ))}
           </ul>
           <p>
             {callsRemaining
--- src/components/CitySelection/CitySelection.js
+++ src/components/CitySelection/CitySelection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import debounce from "../../functions/debounce";
 
 function CitySelection(props) {
   return (
